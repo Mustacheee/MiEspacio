2685c0accd1e2591501880b9e65dea7cece90bd0
Ben de la Torre <ben@dc.targetcw.com>
2016-06-20 17:08:16 -0700

    Edited db name

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=pollos',
+    'dsn' => 'mysql:host=localhost;dbname=MiEspacio',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
